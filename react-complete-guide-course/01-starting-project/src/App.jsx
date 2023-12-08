46eef03412bc4c76030e86a0edd20a0f7e46f515
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-12-08 17:26:14 +0800

    Finish props

--- react-complete-guide-course/01-starting-project/src/App.jsx
+++ react-complete-guide-course/01-starting-project/src/App.jsx
@@ -1,3 +1,6 @@
+import reactImg from "./assets/react-core-concepts.png"
+import componentsImg from "./assets/components.png"
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -9,7 +12,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylised atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are going to build!
@@ -18,12 +21,35 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept 
+            title="Components" 
+            description="The core UI building block."
+            image={componentsImg}
+            />
+          <CoreConcept />
+          <CoreConcept />
+          <CoreConcept />
+        </ul>
+
+        </section>
       </main>
     </div>
   );
