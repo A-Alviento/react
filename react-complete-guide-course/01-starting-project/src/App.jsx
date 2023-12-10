6c79fa92ccd417ea72cd5a4e1be71fc8ecaf3fc9
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-12-10 11:15:34 +0800

    Work with state

--- react-complete-guide-course/01-starting-project/src/App.jsx
+++ react-complete-guide-course/01-starting-project/src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import { CORE_CONCEPTS } from "./data.js";
 import Header from './components/Header/Header.jsx';
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from './components/TabButton.jsx'
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button.')
+
   function handleSelect(selectedButton){
     // selectedButton => 'components', etc.
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton)
+    console.log(selectedTopic);
   }
+
+  console.log('APP COMPONENT EXECUTING')
 
   return (
     <div>
@@ -34,7 +40,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-
+          {selectedTopic}
         </section>
 
       </main>
