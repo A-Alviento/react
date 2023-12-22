84ca54ce5253853ffd394799b1963fa1af62f8ef
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-12-22 12:33:29 +0800

    Add comments

--- redux-medium-todo-app/starter/src/redux/store.js
+++ redux-medium-todo-app/starter/src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit' // configureStore function does all the hard work; it creates the store which holds our state, combines our reducers and has some nice built in middleware
 
 export default configureStore({
     reducer: {}
