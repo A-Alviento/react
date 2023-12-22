7b4fe5528c358d6dd9dd5a5bdd1247721b716f4c
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-12-22 12:45:30 +0800

    Add our todoReducer to the store

--- redux-medium-todo-app/starter/src/redux/store.js
+++ redux-medium-todo-app/starter/src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit' // configureStore function does all the hard work; it creates the store which holds our state, combines our reducers and has some nice built in middleware
+import todoReducer from './todoSlice'; // import the todoSlice reducer function
 
 export default configureStore({
-    reducer: {}
+    reducer: {
+        todos: todoReducer, // add the todoSlice reducer function to the store
+    }
 })
