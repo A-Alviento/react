2c3bdd1231d9a41078960d03264de4c6a27c6fbc
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-12-23 11:35:26 +0800

    Add new course

---
+++ nodejs-yt-course/01-tut/server.js
@@ -0,0 +1,5 @@
+// how nodejs differs from vanilla js
+// 1) node runs on a server - not in browser (backen not frontend)
+// 2) console is the terminal window
+console.log('hello world') // run in terminal with command `node server`
+// 3) global object instead of window object
